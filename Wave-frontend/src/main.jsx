13e9497f92e7917122dc5777387847e8d72c46d7
radhikagupta25 <radhikaaaguptaaa25@gmail.com>
2026-07-07 23:08:23 +0530

    Initialize frontend

--- Wave-frontend/src/main.jsx
+++ Wave-frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: < LandingPage/>,
+    },
+    {
+        path: "/login",
+        element: < LoginPage/>,
+    },
+    {
+        path: "/signup",
+        element: < SignupPage/>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
